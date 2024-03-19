5fb2f3c2ea6d4ae07f5e2d02c770322c95730805
owoDra <85442566+owoDra@users.noreply.github.com>
2024-03-19 17:00:51 +0900

    データ追加

---
+++ Source/BEDataBase/BEDataBase/GameplayTag/Weapon/BEDataBaseTags_Dagger.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright (C) 2024 owoDra
+
+#include "BEDataBaseTags_Dagger.h"
+
+
+////////////////////////////////////
+// DataBase.Weapon.Dagger
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackRadius_Dagger_Normal_First, "DataBase.Weapon.AttackRadius.Dagger.Normal.First");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackRadius_Dagger_Normal_Second, "DataBase.Weapon.AttackRadius.Dagger.Normal.Second");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackRadius_Dagger_Normal_Third, "DataBase.Weapon.AttackRadius.Dagger.Normal.Third");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackRadius_Dagger_Normal_Four, "DataBase.Weapon.AttackRadius.Dagger.Normal.Four");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackRadius_Dagger_Normal_Five, "DataBase.Weapon.AttackRadius.Dagger.Normal.Five");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackRadius_Dagger_Special_Second, "DataBase.Weapon.AttackRadius.Dagger.Special.Second");
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDistance_Dagger_Normal_First, "DataBase.Weapon.AttackDistance.Dagger.Normal.First");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDistance_Dagger_Normal_Second, "DataBase.Weapon.AttackDistance.Dagger.Normal.Second");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDistance_Dagger_Normal_Third, "DataBase.Weapon.AttackDistance.Dagger.Normal.Third");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDistance_Dagger_Normal_Four, "DataBase.Weapon.AttackDistance.Dagger.Normal.Four");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDistance_Dagger_Normal_Five, "DataBase.Weapon.AttackDistance.Dagger.Normal.Five");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDistance_Dagger_Special_Second, "DataBase.Weapon.AttackDistance.Dagger.Special.Second");
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackSpeed_Dagger_Normal_First, "DataBase.Weapon.AttackSpeed.Dagger.Normal.First");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackSpeed_Dagger_Normal_Second, "DataBase.Weapon.AttackSpeed.Dagger.Normal.Second");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackSpeed_Dagger_Normal_Third, "DataBase.Weapon.AttackSpeed.Dagger.Normal.Third");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackSpeed_Dagger_Normal_Four, "DataBase.Weapon.AttackSpeed.Dagger.Normal.Four");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackSpeed_Dagger_Normal_Five, "DataBase.Weapon.AttackSpeed.Dagger.Normal.Five");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackSpeed_Dagger_Special_Second, "DataBase.Weapon.AttackSpeed.Dagger.Special.Second");
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDamage_Dagger_Normal_First, "DataBase.Weapon.AttackDamage.Dagger.Normal.First");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDamage_Dagger_Normal_Second, "DataBase.Weapon.AttackDamage.Dagger.Normal.Second");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDamage_Dagger_Normal_Third, "DataBase.Weapon.AttackDamage.Dagger.Normal.Third");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDamage_Dagger_Normal_Four, "DataBase.Weapon.AttackDamage.Dagger.Normal.Four");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDamage_Dagger_Normal_Five, "DataBase.Weapon.AttackDamage.Dagger.Normal.Five");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_AttackDamage_Dagger_Special_Second, "DataBase.Weapon.AttackDamage.Dagger.Special.Second");
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_HeadMultiply_Dagger_Normal_First, "DataBase.Weapon.HeadMultiply.Dagger.Normal.First");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_HeadMultiply_Dagger_Normal_Second, "DataBase.Weapon.HeadMultiply.Dagger.Normal.Second");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_HeadMultiply_Dagger_Normal_Third, "DataBase.Weapon.HeadMultiply.Dagger.Normal.Third");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_HeadMultiply_Dagger_Normal_Four, "DataBase.Weapon.HeadMultiply.Dagger.Normal.Four");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_HeadMultiply_Dagger_Normal_Five, "DataBase.Weapon.HeadMultiply.Dagger.Normal.Five");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_HeadMultiply_Dagger_Special_Second, "DataBase.Weapon.HeadMultiply.Dagger.Special.Second");
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_Misc_Dagger_Special_First_MoveDuration, "DataBase.Weapon.Misc.Dagger.Special.First.MoveDuration");
+UE_DEFINE_GAMEPLAY_TAG(TAG_DataBase_Weapon_Misc_Dagger_Special_First_MoveStrength, "DataBase.Weapon.Misc.Dagger.Special.First.MoveStrength");
